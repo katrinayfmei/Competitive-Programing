1e548049b70fc5257359a3cd713a5ed5397897ad
Katrina <88742230+itzwhatever@users.noreply.github.com>
2021-08-24 11:23:53 -0400

    Create J1.py

---
+++ 2019/J1.py
@@ -0,0 +1,16 @@
+a=int(input(""))
+b=int(input(""))
+c=int(input(""))
+d=int(input(""))
+e=int(input(""))
+f=int(input(""))
+
+totA=(a*3)+(b*2)+(c*1)
+totB=(d*3)+(e*2)+(f*1)
+
+if totA>totB:
+  print("A")
+elif totA<totB:
+  print("B")
+elif totA==totB:
+  print("T")
