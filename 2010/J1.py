3ea08d12ba45165aa76e409b94efc2334d7b3999
Katrina <88742230+itzwhatever@users.noreply.github.com>
2022-02-01 11:12:36 -0500

    Create J1.py

---
+++ 2010/J1.py
@@ -0,0 +1,23 @@
+x=int(input(""))
+
+
+if x==1:
+ print("1")
+elif x==2:
+ print("2")
+elif x==3:
+ print("2")
+elif x==4:
+ print("3")
+elif x==5:
+ print("3")
+elif x==6:
+ print("3")
+elif x==7:
+ print("2")
+elif x==8:
+ print("2")
+elif x==9:
+ print("1")
+elif x==10:
+ print("1")
