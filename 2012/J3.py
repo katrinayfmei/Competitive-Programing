dbd7f87fb6ea8afb729050fc230c418953c75113
Katrina <88742230+itzwhatever@users.noreply.github.com>
2022-02-02 14:04:13 -0500

    Create J3.py

---
+++ 2012/J3.py
@@ -0,0 +1,12 @@
+a=int(input(""))
+
+for i in range(a):
+  print("*"*a + "x"*a + "*"*a)
+
+
+for i in range(a):
+  print(" "*a + "x"*a + "x"*a)
+
+
+for i in range(a):
+  print("*"*a + " "*a + "*"*a)
